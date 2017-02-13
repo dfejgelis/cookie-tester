85a5e4a49017a7c003b9f54b5c7c53dcbc1d5e1c
Diego Fejgelis <diegofej@gmail.com>
2017-02-13 20:44:48 -0300

    added nodemon

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-var express = require('express')
-var cookieParser = require('cookie-parser')
+const express = require('express');
+const cookieParser = require('cookie-parser');
 
-var app = express()
-app.use(cookieParser())
+const app = express();
+app.use(cookieParser());
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+const tracking = (req, res) => {
+  // Set cookie
+  res.cookie('foo1', 'cookieValue', {
+    expires: new Date('Mon, 13 Mar 2017 21:37:13 GMT'),
+    path: '/',
+  });
 
-  // Cookies that have not been signed
-  console.log('Cookies: ', req.cookies)
+  res.cookie('foo2', 'cookieValue', {
+    expires: new Date('Mon, 13 Mar 2017 21:37:13 GMT'),
+    path: '/',
+  });
+  res.send(req.cookies);
+  console.log('Setting Cookies...');
+};
 
-  // Cookies that have been signed
-  console.log('Signed Cookies: ', req.signedCookies)
-})
+const convert = (req, res) => {
+  res.send(req.cookies);
+  console.log('Receiving Cookies: ', JSON.stringify(req.cookies, null, 2));
+};
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.get('/', (req, res) => {
+  res.send(req.cookies);
+  // console.log('Cookies: ', req.cookies);
+});
+
+app.get('/tracking', tracking);
+app.get('/convert', convert);
+
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
