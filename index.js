c69aeb3ed7f527a340858f8e93899505ddaf6bc9
Diego Fejgelis <diegofej@gmail.com>
2017-02-13 18:30:00 -0300

    added cookieParser

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var express = require('express')
+var cookieParser = require('cookie-parser')
+
 var app = express()
+app.use(cookieParser())
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
+
+  // Cookies that have not been signed
+  console.log('Cookies: ', req.cookies)
+
+  // Cookies that have been signed
+  console.log('Signed Cookies: ', req.signedCookies)
 })
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
